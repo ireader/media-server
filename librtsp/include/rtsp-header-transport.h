6fe9439ce0151d25b13806fb4012bc74007c3d6e
i guest <iguest@sogou.com>
2014-10-23 11:01:53 +0800

    1. rtsp_header_transport_t split multicast/unicast data 2. fixed rtsp transport port value (ref rfc 3550 port define)

--- librtsp/include/rtsp-header-transport.h
+++ librtsp/include/rtsp-header-transport.h
@@ -5,22 +5,16 @@
 extern "C" {
 #endif
 
-enum { 
-	RTSP_TRANSPORT_UNKNOWN = 0, 
+enum {
 	RTSP_TRANSPORT_UNICAST = 1, 
 	RTSP_TRANSPORT_MULTICAST,
 };
 
 // transport
 enum {
-	RTSP_TRANSPORT_RTP = 1,
+	RTSP_TRANSPORT_RTP_UDP = 1,
+	RTSP_TRANSPORT_RTP_TCP,
 	RTSP_TRANSPORT_RAW,
-};
-
-// transport lower transport
-enum {
-	RTSP_TRANSPORT_UDP = 1, 
-	RTSP_TRANSPORT_TCP
 };
 
 // transport mode
@@ -31,20 +25,29 @@ enum {
 
 struct rtsp_header_transport_t
 {
-	int transport; // RTP/RAW
-	int lower_transport; // TCP/UDP, RTP/AVP default UDP
-	int multicast; // unicast/multicast, default multicast
+	int transport; // RTSP_TRANSPORT_xxx
+	int multicast; // 0-unicast/1-multicast, default multicast
 	char destination[32]; // IPv4/IPv6
 	char source[32]; // IPv4/IPv6
 	int layer; // rtsp setup response only
 	int mode; // PLAY/RECORD, default PLAY, rtsp setup response only
 	int append; // use with RECORD mode only, rtsp setup response only
 	int interleaved1, interleaved2; // rtsp setup response only
-	int ttl; // multicast only
-	unsigned short port1, port2; // RTP only
-	unsigned short client_port1, client_port2; // unicast RTP/RTCP port pair, RTP only
-	unsigned short server_port1, server_port2; // unicast RTP/RTCP port pair, RTP only
-	int ssrc; // RTP only(synchronization source (SSRC) identifier) 4-bytes
+	union rtsp_header_transport_rtp_u
+	{
+		struct rtsp_header_transport_multicast_t
+		{
+			int ttl; // multicast only
+			unsigned short port1, port2; // multicast only
+		} m;
+
+		struct rtsp_header_transport_unicast_t
+		{
+			unsigned short client_port1, client_port2; // unicast RTP/RTCP port pair, RTP only
+			unsigned short server_port1, server_port2; // unicast RTP/RTCP port pair, RTP only
+			int ssrc; // RTP only(synchronization source (SSRC) identifier) 4-bytes
+		} u;
+	} rtp;
 };
 
 /// parse RTSP Transport header
--- librtsp/source/rtsp-header-transport.c
+++ librtsp/source/rtsp-header-transport.c
@@ -27,6 +27,14 @@
 // Transport: RTP/AVP;unicast;client_port=4588-4589;server_port=6256-6257
 // Transport: RTP/AVP;multicast;ttl=127;mode="PLAY",RTP/AVP;unicast;client_port=3456-3457;mode="PLAY"
 
+// RTP Port define
+// RFC 3550: 11. RTP over Network and Transport Protocols (p56)
+// 1. For UDP and similar protocols, RTP should use an even destination port number and
+//    the corresponding RTCP stream should use the next higher (odd) destination port number. 
+// 2. For applications that take a single port number as a parameter and derive the RTP and RTCP port
+//    pair from that number, if an odd number is supplied then the application should replace that
+//    number with the next lower (even) number to use as the base of the port pair.
+
 #include "rtsp-header-transport.h"
 #include "cstringext.h"
 #include "string-util.h"
@@ -66,120 +74,152 @@ int rtsp_header_transport(const char* field, struct rtsp_header_transport_t* t)
 	const char* p = field;
 	size_t n;
 
+	memset(t, 0, sizeof(t));
 	t->multicast = 0; // default unicast
-	t->transport = RTSP_TRANSPORT_RTP;
-	t->lower_transport = RTSP_TRANSPORT_UDP;
+	t->transport = RTSP_TRANSPORT_RTP_UDP;
 
 	while(p && *p)
 	{
 		p1 = string_token(p, TRANSPORT_SPECIAL);
 		n = p1 - p;
 
-		if(11 == n && 0 == strnicmp("RTP/AVP/UDP", p, 11))
+		switch(*p)
 		{
-			t->transport = RTSP_TRANSPORT_RTP;
-			t->lower_transport = RTSP_TRANSPORT_UDP;
+		case 'r':
+		case 'R':
+			if(11 == n && 0 == strnicmp("RTP/AVP/UDP", p, 11))
+			{
+				t->transport = RTSP_TRANSPORT_RTP_UDP;
+			}
+			else if(11 == n && 0 == strnicmp("RTP/AVP/TCP", p, 11))
+			{
+				t->transport = RTSP_TRANSPORT_RTP_TCP;
+			}
+			else if(11 == n && 0 == strnicmp("RAW/RAW/UDP", p, 11))
+			{
+				t->transport = RTSP_TRANSPORT_RAW;
+			}
+			else if(7 == n && 0 == strnicmp("RTP/AVP", p, 7))
+			{
+				t->transport = RTSP_TRANSPORT_RTP_UDP;
+			}
+			break;
+
+		case 'u':
+		case 'U':
+			if(7 == n && 0 == strnicmp("unicast", p, 7))
+			{
+				t->multicast = 0;
+			}
+			break;
+
+		case 'm':
+		case 'M':
+			if(9 == n && 0 == strnicmp("multicast", p, 9))
+			{
+				t->multicast = 1;
+			}
+			else if(n > 5 && 0 == strnicmp("mode=", p, 5))
+			{
+				if( (11==n && 0 == stricmp("\"PLAY\"", p+5)) || (9==n && 0 == stricmp("PLAY", p+5)) )
+					t->mode = RTSP_TRANSPORT_PLAY;
+				else if( (13==n && 0 == stricmp("\"RECORD\"", p+5)) || (11==n && 0 == stricmp("RECORD", p+5)) )
+					t->mode = RTSP_TRANSPORT_RECORD;
+			}
+			break;
+
+		case 'd':
+		case 'D':
+			if(n >= 12 && 0 == strnicmp("destination=", p, 12))
+			{
+				assert(p1 - p < sizeof(t->destination)-1);
+				strncpy(t->destination, p+12, p1-p-12);
+				t->destination[p1-p-12] = '\0';
+			}
+			break;
+
+		case 's':
+		case 'S':
+			if(n >= 7 && 0 == strnicmp("source=", p, 7))
+			{
+				assert(p1 - p < sizeof(t->source)-1);
+				strncpy(t->source, p+7, p1-p-7);
+				t->source[p1-p-7] = '\0';
+			}
+			else if(13 == n && 0 == strnicmp("ssrc=", p, 5))
+			{
+				// unicast only
+				assert(0 == t->multicast);
+				t->rtp.u.ssrc = Hex2Int((const unsigned char*)p+5, 8);
+			}
+			else if(2 == sscanf(p, "server_port=%hu-%hu", &t->rtp.u.server_port1, &t->rtp.u.server_port2))
+			{
+				assert(0 == t->multicast);
+			}
+			else if(1 == sscanf(p, "server_port=%hu", &t->rtp.u.server_port1))
+			{
+				assert(0 == t->multicast);
+				t->rtp.u.server_port1 = t->rtp.u.server_port1 / 2 * 2; // RFC 3550 (p56)
+				t->rtp.u.server_port2 = t->rtp.u.server_port1 + 1;
+			}
+			break;
+
+		case 'a':
+			if(6 == n && 0 == stricmp("append", p))
+			{
+				t->append = 1;
+			}
+			break;
+
+		case 'p':
+			if(2 == sscanf(p, "port=%hu-%hu", &t->rtp.m.port1, &t->rtp.m.port2))
+			{
+				assert(1 == t->multicast);
+			}
+			else if(1 == sscanf(p, "port=%hu", &t->rtp.m.port1))
+			{
+				assert(1 == t->multicast);
+				t->rtp.m.port1 = t->rtp.m.port1 / 2 * 2; // RFC 3550 (p56)
+				t->rtp.m.port2 = t->rtp.m.port1 + 1;
+			}
+			break;
+
+		case 'c':
+			if(2 == sscanf(p, "client_port=%hu-%hu", &t->rtp.u.client_port1, &t->rtp.u.client_port2))
+			{
+				assert(0 == t->multicast);
+			}
+			else if(1 == sscanf(p, "client_port=%hu", &t->rtp.u.client_port1))
+			{
+				assert(0 == t->multicast);
+				t->rtp.u.client_port1 = t->rtp.u.client_port1 / 2 * 2; // RFC 3550 (p56)
+				t->rtp.u.client_port2 = t->rtp.u.client_port1 + 1;
+			}
+			break;
+
+		case 'i':
+			if(2 == sscanf(p, "interleaved=%d-%d", &t->interleaved1, &t->interleaved2))
+			{
+			}
+			else if(1 == sscanf(p, "interleaved=%d", &t->interleaved1))
+			{
+				t->interleaved2 = t->interleaved1 + 1;
+			}
+			break;
+
+		case 't':
+			if(1 == sscanf(p, "ttl=%d", &t->rtp.m.ttl))
+			{
+				assert(1 == t->multicast);
+			}
+			break;
+
+		case 'l':
+			if(1 == sscanf(p, "layers=%d", &t->layer))
+			{
+			}
+			break;
 		}
-		else if(11 == n && 0 == strnicmp("RTP/AVP/TCP", p, 11))
-		{
-			t->transport = RTSP_TRANSPORT_RTP;
-			t->lower_transport = RTSP_TRANSPORT_TCP;
-		}
-		else if(11 == n && 0 == strnicmp("RAW/RAW/UDP", p, 11))
-		{
-			t->transport = RTSP_TRANSPORT_RAW;
-			t->lower_transport = RTSP_TRANSPORT_UDP;
-		}
-		else if(7 == n && 0 == strnicmp("RTP/AVP", p, 7))
-		{
-			t->transport = RTSP_TRANSPORT_RTP;
-			t->lower_transport = RTSP_TRANSPORT_UDP;
-		}
-		else if(7 == n && 0 == strnicmp("unicast", p, 7))
-		{
-			t->multicast = 0;
-		}
-		else if(9 == n && 0 == strnicmp("multicast", p, 9))
-		{
-			t->multicast = 1;
-		}
-		else if(n >= 12 && 0 == strnicmp("destination=", p, 12))
-		{
-			assert(p1 - p < sizeof(t->destination)-1);
-			strncpy(t->destination, p+12, p1-p-12);
-			t->destination[p1-p-12] = '\0';
-		}
-	   else if(n >= 7 && 0 == strnicmp("source=", p, 7))
-	   {
-		   assert(p1 - p < sizeof(t->source)-1);
-		   strncpy(t->source, p+7, p1-p-7);
-		   t->source[p1-p-7] = '\0';
-	   }
-	   else if(13 == n && 0 == strnicmp("ssrc=", p, 5))
-	   {
-		   // unicast only
-		   assert(0 == t->multicast);
-		   t->ssrc = Hex2Int((const unsigned char*)p+5, 8);
-	   }
-	   else if(n > 5 && 0 == strnicmp("mode=", p, 5))
-	   {
-		   if( (11==n && 0 == stricmp("\"PLAY\"", p+5)) || (9==n && 0 == stricmp("PLAY", p+5)) )
-			   t->mode = RTSP_TRANSPORT_PLAY;
-		   else if( (13==n && 0 == stricmp("\"RECORD\"", p+5)) || (11==n && 0 == stricmp("RECORD", p+5)) )
-			   t->mode = RTSP_TRANSPORT_RECORD;
-		   else
-			   t->mode = RTSP_TRANSPORT_UNKNOWN;
-	   }
-	   else if(6 == n && 0 == stricmp("append", p))
-	   {
-		   t->append = 1;
-	   }
-	   else if(2 == sscanf(p, "port=%hu-%hu", &t->port1, &t->port2))
-	   {
-		   assert(1 == t->multicast);
-	   }
-	   else if(1 == sscanf(p, "port=%hu", &t->port1))
-	   {
-		   assert(1 == t->multicast);
-		   t->port2 = t->port1 + 1;
-	   }
-	   else if(2 == sscanf(p, "client_port=%hu-%hu", &t->client_port1, &t->client_port2))
-	   {
-		   assert(0 == t->multicast);
-	   }
-	   else if(1 == sscanf(p, "client_port=%hu", &t->client_port1))
-	   {
-		   assert(0 == t->multicast);
-		   t->client_port2 = t->client_port1 + 1;
-	   }
-	   else if(2 == sscanf(p, "server_port=%hu-%hu", &t->server_port1, &t->server_port2))
-	   {
-		   assert(0 == t->multicast);
-	   }
-	   else if(1 == sscanf(p, "server_port=%hu", &t->server_port1))
-	   {
-		   assert(0 == t->multicast);
-		   t->server_port2 = t->server_port1 + 1;
-	   }
-	   else if(2 == sscanf(p, "interleaved=%d-%d", &t->interleaved1, &t->interleaved2))
-	   {
-	   }
-	   else if(1 == sscanf(p, "interleaved=%d", &t->interleaved1))
-	   {
-		   t->interleaved2 = t->interleaved1 + 1;
-	   }
-	   else if(1 == sscanf(p, "ttl=%d", &t->ttl))
-	   {
-		   assert(1 == t->multicast);
-	   }
-	   else if(1 == sscanf(p, "layers=%d", &t->layer))
-	   {
-		   assert(1 == t->multicast);
-	   }
-	   else
-	   {
-		   assert(0); // unknown parameter
-	   }
 
 	   if('\r' == *p1 || '\n' == *p1 || '\0' == *p1)
 		   break;
@@ -196,7 +236,12 @@ void rtsp_header_transport_test()
 
 	memset(&t, 0, sizeof(t));
 	assert(0 == rtsp_header_transport("RTP/AVP;unicast;client_port=4588-4589;server_port=6256-6257", &t)); // rfc2326 p61
-	assert(t.transport==RTSP_TRANSPORT_RTP && t.lower_transport==RTSP_TRANSPORT_UDP);
-	assert(t.multicast==0 && t.client_port1==4588 && t.client_port2==4589 && t.server_port1==6256 && t.server_port2==6257);
+	assert(t.transport==RTSP_TRANSPORT_RTP_UDP);
+	assert(t.multicast==0 && t.rtp.u.client_port1==4588 && t.rtp.u.client_port2==4589 && t.rtp.u.server_port1==6256 && t.rtp.u.server_port2==6257);
+
+	memset(&t, 0, sizeof(t));
+	assert(0 == rtsp_header_transport("RTP/AVP;multicast;ttl=127;mode=\"PLAY\"", &t)); // rfc2326 p61
+	assert(t.transport==RTSP_TRANSPORT_RTP_UDP);
+	assert(t.multicast==1 && 127==t.rtp.m.ttl && RTSP_TRANSPORT_PLAY==t.mode);
 }
 #endif
