f45599b958f72c006b42163814f240aa8a0df194
ireader <ch.magus@163.com>
2016-10-29 00:13:09 +0800

    mpeg2 ts PTS resort demo

--- libmpeg/test/mpeg-ts-dec-test.cpp
+++ libmpeg/test/mpeg-ts-dec-test.cpp
@@ -1,19 +1,37 @@
 #include "mpeg-ps.h"
 #include "mpeg-ts.h"
+#include "mpeg-ts-proto.h"
 #include <stdio.h>
+
+static FILE* vfp;
+static FILE* afp;
 
 static void ts_packet(void* param, int avtype, int64_t pts, int64_t dts, void* data, size_t bytes)
 {
 	printf("[%d] pts: %lld, dts: %lld\n", avtype, pts, dts);
+
+	if (PSI_STREAM_AAC == avtype)
+	{
+		fwrite(data, 1, bytes, afp);
+	}
+	else if (PSI_STREAM_H264 == avtype)
+	{
+		fwrite(data, 1, bytes, vfp);
+	}
 }
 
 void mpeg_ts_dec_test(const char* file)
 {
 	unsigned char ptr[188];
 	FILE* fp = fopen(file, "rb");
+	vfp = fopen("v.h264", "wb");
+	afp = fopen("a.aac", "wb");
+
 	while (1 == fread(ptr, sizeof(ptr), 1, fp))
 	{
 		mpeg_ts_packet_dec(ptr, sizeof(ptr), ts_packet, NULL);
 	}
 	fclose(fp);
+	fclose(vfp);
+	fclose(afp);
 }
--- libmpeg/test/mpeg-ts-test.cpp
+++ libmpeg/test/mpeg-ts-test.cpp
@@ -1,7 +1,28 @@
 #include "mpeg-ps.h"
 #include "mpeg-ts.h"
+#include "mpeg-ts-proto.h"
 #include <assert.h>
 #include <stdio.h>
+#include <list>
+#include <memory.h>
+
+struct mpeg_ts_test_t
+{
+	struct audio
+	{
+		std::auto_ptr<char> data;
+		size_t bytes;
+		int64_t pts;
+		int64_t dts;
+		audio(std::auto_ptr<char>& ptr, size_t bytes, int64_t pts, int64_t dts)
+			:data(ptr), bytes(bytes), pts(pts), dts(dts)
+		{
+		}
+	};
+
+	std::list<audio> audios;
+	void* ts;
+};
 
 static void* ts_alloc(void* /*param*/, size_t bytes)
 {
@@ -24,7 +45,32 @@ extern  "C" size_t mpeg_ts_h264(void* h264, size_t bytes);
 
 static void ts_packet(void* param, int avtype, int64_t pts, int64_t dts, void* data, size_t bytes)
 {
-	mpeg_ts_write(param, avtype, pts, dts, data, bytes);
+	struct mpeg_ts_test_t* ctx = (struct mpeg_ts_test_t*)param;
+
+	if (PSI_STREAM_AAC == avtype)
+	{
+		std::auto_ptr<char> ptr(new char[bytes]);
+		memcpy(ptr.get(), data, bytes);
+		ctx->audios.push_back(mpeg_ts_test_t::audio(ptr, bytes, pts, dts));
+	}
+	else if (PSI_STREAM_H264 == avtype)
+	{
+		while (!ctx->audios.empty())
+		{
+			const mpeg_ts_test_t::audio& audio = ctx->audios.front();
+			if (audio.pts > pts)
+				break;
+
+			mpeg_ts_write(ctx->ts, PSI_STREAM_AAC, audio.pts, audio.dts, audio.data.get(), audio.bytes);
+			ctx->audios.pop_front();
+		}
+
+		mpeg_ts_write(ctx->ts, avtype, pts, dts, data, bytes);
+	}
+	else
+	{
+		assert(0);
+	}
 }
 
 static void mpeg_ts_file(const char* file, void* ts)
@@ -38,17 +84,21 @@ static void mpeg_ts_file(const char* file, void* ts)
 	fclose(fp);
 }
 
-void mpeg_ts_test()
+//mpeg_ts_test("test/apple.ts", "test/fileSequence0.ts")
+void mpeg_ts_test(const char* input, const char* output)
 {
-	FILE* fp = fopen("test/apple.ts", "wb");
+	struct mpeg_ts_test_t ctx;
+
 	struct mpeg_ts_func_t tshandler;
 	tshandler.alloc = ts_alloc;
 	tshandler.write = ts_write;
 	tshandler.free = ts_free;
-	void* ts = mpeg_ts_create(&tshandler, fp);
-
-	mpeg_ts_file("test/fileSequence0.ts", ts);
 	
-	mpeg_ts_destroy(ts);
+	FILE* fp = fopen(output, "wb");
+	ctx.ts = mpeg_ts_create(&tshandler, fp);
+
+	mpeg_ts_file(input, &ctx);
+	
+	mpeg_ts_destroy(ctx.ts);
 	fclose(fp);
 }
