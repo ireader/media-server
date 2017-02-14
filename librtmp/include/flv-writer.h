2ac459831cfe80d64fd0894153342a4cd9812932
ireader <ch.magus@163.com>
2017-02-14 17:54:26 +0800

    describe FLV write audio/video stream format

--- librtmp/include/flv-writer.h
+++ librtmp/include/flv-writer.h
@@ -12,8 +12,10 @@ void* flv_writer_create(const char* file);
 
 void flv_writer_destroy(void* flv);
 
+/// @param[in] data AAC ADTS stream, 0xFFF15C40011FFC...
 int flv_writer_audio(void* flv, const void* data, size_t bytes, uint32_t pts, uint32_t dts);
 
+// @param[in] data H.264 start code + H.264 NALU, 0x0000000168...
 int flv_writer_video(void* flv, const void* data, size_t bytes, uint32_t pts, uint32_t dts);
 
 #if defined(__cplusplus)
