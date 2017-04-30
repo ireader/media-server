34ef252aee707b0ac5c54e48f03ab778cf6d1d43
ireader <ch.magus@163.com>
2017-04-30 15:21:03 +0800

    flv to TS/HLS convert

---
+++ libflv/test/flv2ts-test.cpp
@@ -0,0 +1,111 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "flv-demuxer.h"
+#include "flv-reader.h"
+#include "../libmpeg/include/mpeg-ps.h"
+#include "../libmpeg/include/mpeg-ts.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+static void* ts_alloc(void* /*param*/, size_t bytes)
+{
+	static char s_buffer[188];
+	assert(bytes <= sizeof(s_buffer));
+	return s_buffer;
+}
+
+static void ts_free(void* /*param*/, void* /*packet*/)
+{
+	return;
+}
+
+static void ts_write(void* param, const void* packet, size_t bytes)
+{
+	fwrite(packet, bytes, 1, (FILE*)param);
+}
+
+inline const char* ftimestamp(uint32_t t, char* buf)
+{
+	sprintf(buf, "%u:%02u:%02u.%03u", t / 36000000, (t / 60000) % 60, (t / 1000) % 60, t % 1000);
+	return buf;
+}
+
+inline char flv_type(int type)
+{
+	switch (type)
+	{
+	case FLV_AAC: return 'A';
+	case FLV_MP3: return 'M';
+	case FLV_AVC: return 'V';
+	case FLV_AAC_HEADER: return 'a';
+	case FLV_AVC_HEADER: return 'v';
+	default: return '*';
+	}
+}
+
+static void onFLV(void* ts, int type, const void* data, size_t bytes, unsigned int pts, unsigned int dts)
+{
+	static char s_pts[64], s_dts[64];
+	static uint32_t v_pts = 0, v_dts = 0;
+	static uint32_t a_pts = 0, a_dts = 0;
+
+	printf("[%c] pts: %s, dts: %s, ", flv_type(type), ftimestamp(pts, s_pts), ftimestamp(dts, s_dts));
+
+	if (FLV_AAC == type)
+	{
+		//		assert(0 == a_dts || dts >= a_dts);
+		pts = (a_pts && pts < a_pts) ? a_pts : pts;
+		dts = (a_dts && dts < a_dts) ? a_dts : dts;
+		mpeg_ts_write(ts, STREAM_AUDIO_AAC, pts * 90, dts * 90, data, bytes);
+
+		printf("diff: %03d/%03d", (int)(pts - a_pts), (int)(dts - a_dts));
+		a_pts = pts;
+		a_dts = dts;
+	}
+	else if (FLV_AVC == type)
+	{
+		assert(0 == v_dts || dts >= v_dts);
+		dts = (a_dts && dts < v_dts) ? v_dts : dts;
+		mpeg_ts_write(ts, STREAM_VIDEO_H264, pts * 90, dts * 90, data, bytes);
+
+		printf("diff: %03d/%03d", (int)(pts - v_pts), (int)(dts - v_dts));
+		v_pts = pts;
+		v_dts = dts;
+	}
+	else
+	{
+		// nothing to do
+	}
+
+	printf("\n");
+}
+
+void flv2ts_test(const char* inputFLV, const char* outputTS)
+{
+	struct mpeg_ts_func_t tshandler;
+	tshandler.alloc = ts_alloc;
+	tshandler.write = ts_write;
+	tshandler.free = ts_free;
+
+	FILE* fp = fopen(outputTS, "wb");
+	void* ts = mpeg_ts_create(&tshandler, fp);
+	void* reader = flv_reader_create(inputFLV);
+	void* flv = flv_demuxer_create(onFLV, ts);
+
+	int type, r = 0;
+	uint32_t timestamp;
+	static unsigned char s_packet[8 * 1024 * 1024];
+	while ((r = flv_reader_read(reader, &type, &timestamp, s_packet, sizeof(s_packet))) > 0)
+	{
+		r = flv_demuxer_input(flv, type, s_packet, r, timestamp);
+		if (r < 0)
+		{
+			assert(0);
+		}
+	}
+
+	flv_demuxer_destroy(flv);
+	flv_reader_destroy(reader);
+	mpeg_ts_destroy(ts);
+	fclose(fp);
+}
---
+++ libflv/test/ts2flv-test.cpp
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "flv-writer.h"
+#include "flv-muxer.h"
+#include "../libmpeg/include/mpeg-ts.h"
+#include "../libmpeg/include/mpeg-ts-proto.h"
+#include <stdio.h>
+#include <assert.h>
+
+static void on_flv_packet(void* flv, int type, const void* data, size_t bytes, uint32_t timestamp)
+{
+	flv_writer_input(flv, type, data, bytes, timestamp);
+}
+
+static void ts_packet(void* muxer, int avtype, int64_t pts, int64_t dts, void* data, size_t bytes)
+{
+	static int64_t s_pts = 0;
+	if (0 == s_pts)
+		s_pts = pts;
+	pts -= s_pts;
+	dts -= s_pts;
+
+	if (PSI_STREAM_AAC == avtype)
+	{
+		flv_muxer_audio(muxer, data, bytes, (uint32_t)(pts/90), (uint32_t)(pts / 90));
+	}
+	else if (PSI_STREAM_H264 == avtype)
+	{
+		flv_muxer_video(muxer, data, bytes, (uint32_t)(pts / 90), (uint32_t)(pts / 90));
+	}
+}
+
+void ts2flv_test(const char* inputTS, const char* outputFLV)
+{
+	void* f = flv_writer_create(outputFLV);
+	void* m = flv_muxer_create(on_flv_packet, f);
+
+	unsigned char ptr[188];
+	FILE* fp = fopen(inputTS, "rb");
+	while (1 == fread(ptr, sizeof(ptr), 1, fp))
+	{
+		mpeg_ts_packet_dec(ptr, sizeof(ptr), ts_packet, m);
+	}
+	fclose(fp);
+
+	flv_muxer_destroy(m);
+	flv_writer_destroy(f);
+}
