653ce8a1dda782ad87ac9f62a7b4c0a937921dbd
ireader <ch.magus@163.com>
2017-05-04 18:08:43 +0800

    HLS support encoder

--- libhls/include/hls-param.h
+++ libhls/include/hls-param.h
@@ -1,7 +1,16 @@
 #ifndef _hls_param_h_
 #define _hls_param_h_
 
+// https://developer.apple.com/library/content/documentation/NetworkingInternet/Conceptual/StreamingMediaGuide/FrequentlyAskedQuestions/FrequentlyAskedQuestions.html
 // https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/StreamingMediaGuide/FrequentlyAskedQuestions/FrequentlyAskedQuestions.html
+// 1. What kinds of encoders are supported?
+// H.264 video and AAC audio (HE-AAC or AAC-LC).
+// 2. What are the specifics of the video and audio formats supported?
+// Video: H.264 Baseline Level 3.0, Baseline Level 3.1, Main Level 3.1, and High Profile Level 4.1.
+// Audio: HE-AAC or AAC-LC up to 48 kHz, stereo audio
+//        MP3 (MPEG-1 Audio Layer 3) 8 kHz to 48 kHz, stereo audio
+//        AC-3 (for Apple TV, in pass-through mode only)
+
 // 3. What duration should media files be?
 // A duration of 10 seconds of media per file seems to strike a reasonable balance for most broadcast content.
 // http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8
