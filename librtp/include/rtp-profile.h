b9997bfa7328fd5d4a6c1174e91dd5e443c7a724
ireader <ch.magus@163.com>
2017-05-08 23:36:30 +0800

    define MPEG1/MPEG2 audio/video elementary stream payload type

--- librtp/include/rtp-profile.h
+++ librtp/include/rtp-profile.h
@@ -1,6 +1,7 @@
 #ifndef _rtp_profile_h_
 #define _rtp_profile_h_
 
+/// https://en.wikipedia.org/wiki/RTP_audio_video_profile
 /// RFC3551 6. Payload Type Definitions (p28)
 struct rtp_profile_t
 {
@@ -72,17 +73,22 @@ struct rtp_profile_t
 
 enum
 {
-	RTP_PAYLOAD_G711		= 0,
-	RTP_PAYLOAD_G722		= 9,
-	RTP_PAYLOAD_G729		= 18,
+	RTP_PAYLOAD_G711U		= 0,  // ITU-T G.711 PCM µ-Law audio 64 kbit/s (rfc3551)
+	RTP_PAYLOAD_G722		= 9,  // ITU-T G.722 audio 64 kbit/s (rfc3551)
+	RTP_PAYLOAD_G729		= 18, // ITU-T G.729 and G.729a audio 8 kbit/s (rfc3551)
+	RTP_PAYLOAD_MPA			= 14, // MPEG-1/MPEG-2 audio (rfc2250)
+	RTP_PAYLOAD_MP4A		= 99, // MP4A-LATM MPEG-4 Audio (rfc6416)
 
-	RTP_PAYLOAD_MPEG2TS		= 33,
-	RTP_PAYLOAD_MPEG2PS		= 97,
-	RTP_PAYLOAD_JPEG		= 26,
-	RTP_PAYLOAD_MPEG2		= 32,
-	RTP_PAYLOAD_MPEG4		= 96,
-	RTP_PAYLOAD_H263		= 34,
-	RTP_PAYLOAD_H264		= 98,
+	RTP_PAYLOAD_JPEG		= 26, // JPEG video (rfc2435)
+	RTP_PAYLOAD_MPV			= 32, // MPEG-1 and MPEG-2 video (rfc2250)
+	RTP_PAYLOAD_MP2T		= 33, // MPEG-2 transport stream (rfc2250)
+	RTP_PAYLOAD_H263		= 34, // H.263 video, first version (1996) (rfc2190)
+
+	RTP_PAYLOAD_MP4V		= 96, // MP4V-ES MPEG-4 Visual (rfc6416)
+	RTP_PAYLOAD_H264		= 97, // H.264 video (MPEG-4 Part 10) (rfc6184)
+	RTP_PAYLOAD_MP2P		= 98, // MPEG-2 Program Streams video (rfc2250)
+	RTP_PAYLOAD_MP4ES		= 100, // MPEG4-generic audio/video MPEG-4 Elementary Streams (rfc3640)
+
 };
 
 #endif /* _rtp_profile_h_ */
