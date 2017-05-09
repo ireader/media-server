9bd90238ca087d2046c387674ca0aeac892216cd
ireader <ch.magus@163.com>
2017-05-09 18:13:07 +0800

    MP4/H.264 annexb steam convert

---
+++ libflv/source/mpeg4-annexbtomp4.c
@@ -0,0 +1,106 @@
+// ISO/IEC 14496-1:2010(E)
+// Annex I: Usage of ITU-T Recommendation H.264 | ISO/IEC 14496-10 AVC (p150)
+//
+// 1. Start Codes shall not be present in the stream. The field indicating the size of each following NAL unit
+//    shall be added before NAL unit.The size of this field is defined in DecoderSpecificInfo.
+// 2. It is recommended encapsulating one NAL unit in one SL packet when it is delivered over lossy environment.
+
+#include "h264-util.h"
+#include "mpeg4-avc.h"
+#include <string.h>
+#include <assert.h>
+#include <stdint.h>
+#include <stddef.h>
+
+#define H264_NAL_IDR		5 // Coded slice of an IDR picture
+#define H264_NAL_SPS		7 // Sequence parameter set
+#define H264_NAL_PPS		8 // Picture parameter set
+
+struct mpeg4_handle_t
+{
+	struct mpeg4_avc_t* avc;
+	int errcode;
+
+	uint8_t* ptr;
+	size_t bytes;
+	size_t capacity;
+};
+
+static void h264_handler(void* param, const void* nalu, size_t bytes)
+{
+	struct mpeg4_handle_t* mp4;	
+	mp4 = (struct mpeg4_handle_t*)param;
+
+	switch (((unsigned char*)nalu)[0] & 0x1f)
+	{
+	case H264_NAL_SPS:
+		assert(bytes <= sizeof(mp4->avc->sps[mp4->avc->nb_sps].data));
+		if (bytes <= sizeof(mp4->avc->sps[mp4->avc->nb_sps].data))
+		{
+			mp4->avc->sps[mp4->avc->nb_sps].bytes = (uint16_t)bytes;
+			memcpy(mp4->avc->sps[mp4->avc->nb_sps].data, nalu, bytes);
+			++mp4->avc->nb_sps;
+		}
+		else
+		{
+			mp4->errcode = -1;
+		}
+
+		if (1 == mp4->avc->nb_sps)
+		{
+			mp4->avc->profile = mp4->avc->sps[0].data[1];
+			mp4->avc->compatibility = mp4->avc->sps[0].data[2];
+			mp4->avc->level = mp4->avc->sps[0].data[3];
+		}
+		return;
+
+	case H264_NAL_PPS:
+		assert(bytes <= sizeof(mp4->avc->pps[mp4->avc->nb_pps].data));
+		if (bytes <= sizeof(mp4->avc->pps[mp4->avc->nb_pps].data))
+		{
+			mp4->avc->pps[mp4->avc->nb_pps].bytes = (uint16_t)bytes;
+			memcpy(mp4->avc->pps[mp4->avc->nb_pps].data, nalu, bytes);
+			++mp4->avc->nb_pps;
+		}
+		else
+		{
+			mp4->errcode = -1;
+		}
+		return;
+
+	case H264_NAL_IDR:
+		mp4->avc->chroma_format_idc = 1;
+		break;
+
+	//case H264_NAL_SPS_EXTENSION:
+	//case H264_NAL_SPS_SUBSET:
+	//	break;
+	}
+
+	if (mp4->capacity >= mp4->bytes + bytes + 4)
+	{
+		mp4->ptr[mp4->bytes + 0] = (uint8_t)((bytes >> 24) & 0xFF);
+		mp4->ptr[mp4->bytes + 1] = (uint8_t)((bytes >> 16) & 0xFF);
+		mp4->ptr[mp4->bytes + 2] = (uint8_t)((bytes >> 8) & 0xFF);
+		mp4->ptr[mp4->bytes + 3] = (uint8_t)((bytes >> 0) & 0xFF);
+		memcpy(mp4->ptr + mp4->bytes + 4, nalu, bytes);
+		mp4->bytes += bytes + 4;
+	}
+	else
+	{
+		mp4->errcode = -1;
+	}
+}
+
+size_t mpeg4_annexbtomp4(struct mpeg4_avc_t* avc, const void* data, size_t bytes, void* out, size_t size)
+{
+	struct mpeg4_handle_t h;
+	h.avc = avc;
+	h.ptr = (uint8_t*)out;
+	h.capacity = size;
+	h.bytes = 0;
+	h.errcode = 0;
+	avc->nalu = 4;
+	h264_stream(data, bytes, h264_handler, &h);
+	return 0 == h.errcode ? h.bytes : 0;
+}
---
+++ libflv/test/flv-read-write-test.cpp
@@ -0,0 +1,53 @@
+#include "flv-reader.h"
+#include "flv-writer.h"
+#include "flv-demuxer.h"
+#include "flv-muxer.h"
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+
+static void flv_onmuxer(void* flv, int type, const void* data, size_t bytes, uint32_t timestamp)
+{
+	flv_writer_input(flv, type, data, bytes, timestamp);
+}
+
+static void flv_ondemuxer(void* flv, int type, const void* data, size_t bytes, unsigned int pts, unsigned int dts)
+{
+	switch (type)
+	{
+	case FLV_AAC:
+	case FLV_MP3:
+		flv_muxer_audio(flv, data, bytes, pts, dts);
+		break;
+
+	case FLV_AVC:
+		flv_muxer_video(flv, data, bytes, pts, dts);
+		break;
+
+	default:
+		break;
+	}
+}
+
+void flv_read_write_test(const char* flv)
+{
+	static char packet[2 * 1024 * 1024];
+	snprintf(packet, sizeof(packet), "%s.flv", flv);
+
+	void* r = flv_reader_create(flv);
+	void* w = flv_writer_create(packet);
+	void* e = flv_muxer_create(flv_onmuxer, w);
+	void* d = flv_demuxer_create(flv_ondemuxer, e);
+
+	int ret, tag;
+	uint32_t timestamp;
+	while ((ret = flv_reader_read(r, &tag, &timestamp, packet, sizeof(packet))) > 0)
+	{
+		flv_demuxer_input(d, tag, packet, ret, timestamp);
+	}
+
+	flv_muxer_destroy(e);
+	flv_demuxer_destroy(d);
+	flv_reader_destroy(r);
+	flv_writer_destroy(w);
+}
