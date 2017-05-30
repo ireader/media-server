3d56ae79d08721243b37667bc6618f35e37524a3
ireader <ch.magus@163.com>
2017-05-30 22:00:21 +0800

    add reference count

--- librtmp/test/rtmp-client-transport.cpp
+++ librtmp/test/rtmp-client-transport.cpp
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <string>
+#include "sys/atomic.h"
 #include "sys/sync.hpp"
 
 enum 
@@ -19,6 +20,8 @@ enum
 
 struct rtmp_client_transport_t
 {
+	int32_t ref;
+
 	int port;
 	char host[128];
 	char app[128];
@@ -30,7 +33,8 @@ struct rtmp_client_transport_t
 	void* rtmp;
 	aio_socket_t socket;
 	int aio_send_status; // AIO_SEND_XXX
-	bool avbuffer_write; 
+	bool avbuffer_write;
+	bool running;
 	bool onready;
 	
 	// recv buffer
@@ -50,6 +54,31 @@ struct rtmp_client_transport_t
 	void* param;
 	struct rtmp_client_transport_handler_t handler;
 };
+
+class rtmp_client_transport_release
+{
+	struct rtmp_client_transport_t* t;
+
+public:
+	rtmp_client_transport_release(struct rtmp_client_transport_t* t) 
+	{
+		this->t = t;
+	}
+
+	~rtmp_client_transport_release() 
+	{
+		if (0 == atomic_decrement32(&t->ref))
+		{
+			delete t;
+		}
+	}
+};
+
+static int rtmp_client_transport_send(struct rtmp_client_transport_t* t);
+static int rtmp_client_transport_recv(struct rtmp_client_transport_t* t);
+static void rtmp_client_transport_onsend(void* transport, int code, size_t bytes);
+static void rtmp_client_transport_onrecv(void* transport, int code, size_t bytes);
+static void rtmp_client_transport_onconnect(void* transport, int code);
 
 static void* rtmp_client_transport_alloc(void* transport, int avtype, size_t bytes)
 {
@@ -126,6 +155,7 @@ void* rtmp_client_transport_create(const char* host, int port, const char* app, 
 
 	memcpy(&t->handler, handler, sizeof(t->handler));
 	t->param = param;
+	t->ref = 1;
 	return t;
 }
 
@@ -134,7 +164,36 @@ void rtmp_client_transport_destroy(void* transport)
 	struct rtmp_client_transport_t* t;
 	t = (struct rtmp_client_transport_t*)transport;
 	rtmp_client_transport_stop(t);
-	delete t;
+	rtmp_client_transport_release release(t);
+}
+
+static int rtmp_client_transport_send(struct rtmp_client_transport_t* t)
+{
+	//assert(t->locker.IsLocked());
+	//AutoThreadLocker locker(t->locker);
+	if (AIO_SEND_IDLE != t->aio_send_status || t->sbytes + t->avbytes < 1)
+		return 0; // busy or not data to send
+
+	assert(t->running);
+	void* buffer = (void*)(t->sbytes > 0 ? t->sbuffer : t->avbuffer);
+	size_t bytes = t->sbytes > 0 ? t->sbytes : t->avbytes;
+	t->aio_send_status = t->sbytes > 0 ? AIO_SEND_PROTOCOL : AIO_SEND_AVMEDIA;
+
+	atomic_increment32(&t->ref);
+	int r = aio_socket_send(t->socket, buffer, bytes, rtmp_client_transport_onsend, t);
+	if (0 != r) atomic_decrement32(&t->ref);
+	assert(t->ref > 0);
+	return r;
+}
+
+static int rtmp_client_transport_recv(struct rtmp_client_transport_t* t)
+{
+	assert(t->running);
+	atomic_increment32(&t->ref);
+	int r = aio_socket_recv(t->socket, t->rbuffer, sizeof(t->rbuffer), rtmp_client_transport_onrecv, t);
+	if (0 != r) atomic_decrement32(&t->ref);
+	assert(t->ref > 0);
+	return r;
 }
 
 static void rtmp_client_transport_onerror(struct rtmp_client_transport_t* t)
@@ -145,41 +204,46 @@ static void rtmp_client_transport_onerror(struct rtmp_client_transport_t* t)
 	rtmp_client_transport_start(t, t->publish);
 }
 
-static int rtmp_client_transport_send(rtmp_client_transport_t* t);
 static void rtmp_client_transport_onsend(void* transport, int code, size_t bytes)
 {
 	struct rtmp_client_transport_t* t;
 	t = (struct rtmp_client_transport_t*)transport;
-	assert(AIO_SEND_IDLE != t->aio_send_status);
-	
-	if (0 == code)
-	{
-		AutoThreadLocker locker(t->locker);
-		if (AIO_SEND_PROTOCOL == t->aio_send_status)
-		{
-			// some body write more data
-			if (t->sbytes > bytes)
-				memmove(t->sbuffer, t->sbuffer + bytes, t->sbytes - bytes);
-			t->sbytes -= bytes;
-		}
-		else if(AIO_SEND_AVMEDIA == t->aio_send_status)
-		{
-			t->avbytes -= bytes;
-			if(0 == t->avbytes)
-				t->handler.onsend(t->param);
-		}
-		else
-		{
-			assert(0);
-		}
+	rtmp_client_transport_release release(t);
 
-		t->aio_send_status = AIO_SEND_IDLE;
-		code = rtmp_client_transport_send(t);
-	}
-	else
 	{
 		AutoThreadLocker locker(t->locker);
+		assert(AIO_SEND_IDLE != t->aio_send_status);
+		int status = t->aio_send_status;
 		t->aio_send_status = AIO_SEND_IDLE;
+		if (!t->running) return;
+
+		if (0 == code)
+		{
+			if (AIO_SEND_PROTOCOL == status)
+			{
+				// some body write more data
+				if (t->sbytes > bytes)
+					memmove(t->sbuffer, t->sbuffer + bytes, t->sbytes - bytes);
+				t->sbytes -= bytes;
+			}
+			else if (AIO_SEND_AVMEDIA == status)
+			{
+				t->avbytes -= bytes;
+				
+				// notify send done
+				int r = t->avbytes;
+				t->locker.Unlock();
+				if (0 == r && t->handler.onsend)
+					t->handler.onsend(t->param);
+				t->locker.Lock();
+			}
+			else
+			{
+				assert(0);
+			}
+
+			code = rtmp_client_transport_send(t);
+		}
 	}
 
 	if (0 != code)
@@ -189,49 +253,41 @@ static void rtmp_client_transport_onsend(void* transport, int code, size_t bytes
 	}
 }
 
-static int rtmp_client_transport_send(rtmp_client_transport_t* t)
-{
-	//assert(t->locker.IsLocked());
-	//AutoThreadLocker locker(t->locker);
-	if (AIO_SEND_IDLE != t->aio_send_status || t->sbytes + t->avbytes < 1)
-		return 0; // busy or not data to send
-
-	void* buffer = (void*)(t->sbytes > 0 ? t->sbuffer : t->avbuffer);
-	size_t bytes = t->sbytes > 0 ? t->sbytes : t->avbytes;
-	t->aio_send_status = t->sbytes > 0 ? AIO_SEND_PROTOCOL : AIO_SEND_AVMEDIA;
-	return aio_socket_send(t->socket, buffer, bytes, rtmp_client_transport_onsend, t);
-}
-
 static void rtmp_client_transport_onrecv(void* transport, int code, size_t bytes)
 {
 	struct rtmp_client_transport_t* t;
 	t = (struct rtmp_client_transport_t*)transport;
+	rtmp_client_transport_release release(t);
 
-	if (0 == code)
+	if (0 == bytes && 0 == code)
 	{
-		if (0 == bytes)
+		// socket close by peer (maybe no data)
+		app_log(LOG_ERROR, "RTMP url(%s) connection close by peer.\n", t->tcurl);
+		rtmp_client_transport_onerror(t);
+		return;
+	}
+
+	{
+		AutoThreadLocker locker(t->locker);
+		if (!t->running) return;
+
+		if (0 == code)
 		{
-			// socket close by peer (maybe no data)
-			app_log(LOG_ERROR, "RTMP url(%s) connection close by peer.\n", t->tcurl);
-			rtmp_client_transport_onerror(t);
-			return;
+			if (0 == rtmp_client_input(t->rtmp, t->rbuffer, bytes))
+				code = rtmp_client_transport_send(t);
 		}
 
-		AutoThreadLocker locker(t->locker);
-		if (0 == rtmp_client_input(t->rtmp, t->rbuffer, bytes))
-			code = rtmp_client_transport_send(t);
-	}
+		if (0 == t->publish && t->onready && 4/*RTMP_STATE_START*/ == rtmp_client_getstate(t->rtmp))
+		{
+			t->onready = false;
+			if (t->handler.onready && t->running)
+				t->handler.onready(t->param);
+		}
 
-	if (0 == t->publish && t->onready && 4/*RTMP_STATE_START*/ == rtmp_client_getstate(t->rtmp))
-	{
-		t->onready = false;
-		if(t->handler.onready)
-			t->handler.onready(t->param);
+		// recv more data
+		if (0 == code)
+			code = rtmp_client_transport_recv(t);
 	}
-
-	// recv more data
-	if (0 == code)
-		code = aio_socket_recv(t->socket, t->rbuffer, sizeof(t->rbuffer), rtmp_client_transport_onrecv, t);
 
 	if (0 != code)
 	{
@@ -244,34 +300,22 @@ static void rtmp_client_transport_onconnect(void* transport, int code)
 {
 	struct rtmp_client_transport_t* t;
 	t = (struct rtmp_client_transport_t*)transport;
+	rtmp_client_transport_release release(t);
 
-	if (0 == code)
 	{
-		struct rtmp_client_handler_t handler;
-		memset(&handler, 0, sizeof(handler));
-		handler.send = rtmp_client_transport_write;
-		handler.alloc = rtmp_client_transport_alloc;
-		handler.onmeta = rtmp_client_transport_onmeta;
-		handler.onaudio = rtmp_client_transport_onaudio;
-		handler.onvideo = rtmp_client_transport_onvideo;
-
-		assert(NULL == t->rtmp);
-		t->rtmp = rtmp_client_create(t->app, t->stream, t->tcurl, t, &handler);
-		if (NULL == t->rtmp)
-		{
-			app_log(LOG_ERROR, "RTMP url(%s) create error.\n", t->tcurl);
-			rtmp_client_transport_onerror(t);
-			return;
-		}
-		
 		AutoThreadLocker locke(t->locker);
-		if (0 == rtmp_client_start(t->rtmp, t->publish))
-			code = rtmp_client_transport_send(t);
-	}
+		if (!t->running) return;
 
-	// start recv
-	if(0 == code)
-		code = aio_socket_recv(t->socket, t->rbuffer, sizeof(t->rbuffer), rtmp_client_transport_onrecv, t);
+		if (0 == code)
+		{
+			if (0 == rtmp_client_start(t->rtmp, t->publish))
+				code = rtmp_client_transport_send(t);
+		}
+
+		// start recv
+		if (0 == code)
+			code = rtmp_client_transport_recv(t);
+	}
 
 	if (0 != code)
 	{
@@ -280,7 +324,7 @@ static void rtmp_client_transport_onconnect(void* transport, int code)
 	}
 }
 
-static void rtmp_client_transport_connect(struct rtmp_client_transport_t* t)
+static int rtmp_client_transport_connect(struct rtmp_client_transport_t* t)
 {
 	socket_t tcp = socket_tcp();
 #if defined(OS_WINDOWS)
@@ -294,9 +338,8 @@ static void rtmp_client_transport_connect(struct rtmp_client_transport_t* t)
 	if (invalid_aio_socket == t->socket)
 	{
 		app_log(LOG_ERROR, "RTMP url(%s) create socket error: %d\n", t->tcurl, socket_geterror());
-		rtmp_client_transport_onerror(t);
 		socket_close(tcp);
-		return;
+		return -1;
 	}
 
 	struct sockaddr_storage ss;
@@ -304,17 +347,19 @@ static void rtmp_client_transport_connect(struct rtmp_client_transport_t* t)
 	if (0 != socket_addr_from(&ss, &len, t->host, (u_short)t->port))
 	{
 		app_log(LOG_ERROR, "RTMP url(%s) get address error: %d\n", t->tcurl, socket_geterror());
-		rtmp_client_transport_onerror(t);
-		return;
+		return -1;
 	}
 
+	atomic_increment32(&t->ref);
 	int r = aio_socket_connect(t->socket, (sockaddr*)&ss, len, rtmp_client_transport_onconnect, t);
 	if (0 != r)
 	{
 		app_log(LOG_ERROR, "RTMP url(%s) connect error: %d/%d\n", t->tcurl, r, socket_geterror());
-		rtmp_client_transport_onerror(t);
-		return;
+		atomic_decrement32(&t->ref);
+		return -1;
 	}
+
+	return 0;
 }
 
 int rtmp_client_transport_start(void* transport, int publish)
@@ -323,14 +368,31 @@ int rtmp_client_transport_start(void* transport, int publish)
 	t = (struct rtmp_client_transport_t*)transport;
 	t->publish = publish;
 	t->onready = true;
-	rtmp_client_transport_connect(t);
-	return 0;
+	t->running = true;
+
+	assert(NULL == t->rtmp);
+	struct rtmp_client_handler_t handler;
+	memset(&handler, 0, sizeof(handler));
+	handler.send = rtmp_client_transport_write;
+	handler.alloc = rtmp_client_transport_alloc;
+	handler.onmeta = rtmp_client_transport_onmeta;
+	handler.onaudio = rtmp_client_transport_onaudio;
+	handler.onvideo = rtmp_client_transport_onvideo;
+	t->rtmp = rtmp_client_create(t->app, t->stream, t->tcurl, t, &handler);
+	if (NULL == t->rtmp)
+		return -1;
+
+	return rtmp_client_transport_connect(t);
 }
 
 int rtmp_client_transport_stop(void* transport)
 {
 	struct rtmp_client_transport_t* t;
 	t = (struct rtmp_client_transport_t*)transport;
+
+	AutoThreadLocker locker(t->locker);
+	t->onready = false;
+	t->running = false;
 
 	if (invalid_aio_socket != t->socket)
 	{
@@ -347,7 +409,7 @@ int rtmp_client_transport_stop(void* transport)
 	return 0;
 }
 
-int rtmp_client_transport_avalloc(rtmp_client_transport_t* t, size_t bytes)
+static int rtmp_client_transport_avalloc(rtmp_client_transport_t* t, size_t bytes)
 {
 	assert(0 == t->avbytes);
 	if (t->avcapacity < bytes + 2 * 1024)
