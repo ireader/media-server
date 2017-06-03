863ba5f1798c08c13b239a4953cfa23afc253914
ireader <ch.magus@163.com>
2017-06-03 22:31:22 +0800

    AAC SBR ASC test case

--- libflv/source/mpeg4-aac.c
+++ libflv/source/mpeg4-aac.c
@@ -126,15 +126,16 @@ int mpeg4_aac_audio_frequency_from(int frequence)
 #if defined(_DEBUG) || defined(DEBUG)
 void mpeg4_aac_test(void)
 {
-	const unsigned char src[] = { 0x13, 0x88 };
+	struct mpeg4_aac_t aac, aac2;
+	const unsigned char asc[] = { 0x13, 0x88 };
 	const unsigned char adts[] = { 0xFF, 0xF1, 0x5C, 0x40, 0x01, 0x1F, 0xFC };
+//	const unsigned char ascsbr[] = { 0x13, 0x10, 0x56, 0xe5, 0x9d, 0x48, 0x00 };
 	unsigned char data[8];
 
-	struct mpeg4_aac_t aac, aac2;
-	assert(sizeof(src) == mpeg4_aac_audio_specific_config_load(src, sizeof(src), &aac));
+	assert(sizeof(asc) == mpeg4_aac_audio_specific_config_load(asc, sizeof(asc), &aac));
 	assert(2 == aac.profile && 7 == aac.sampling_frequency_index && 1 == aac.channel_configuration);
-	assert(sizeof(src) == mpeg4_aac_audio_specific_config_save(&aac, data, sizeof(data)));
-	assert(0 == memcmp(src, data, sizeof(src)));
+	assert(sizeof(asc) == mpeg4_aac_audio_specific_config_save(&aac, data, sizeof(data)));
+	assert(0 == memcmp(asc, data, sizeof(asc)));
 
 	assert(sizeof(adts) == mpeg4_aac_adts_save(&aac, 1, data, sizeof(data)));
 	assert(0 == memcmp(adts, data, sizeof(adts)));
@@ -143,5 +144,8 @@ void mpeg4_aac_test(void)
 
 	assert(22050 == mpeg4_aac_audio_frequency_to(aac.sampling_frequency_index));
 	assert(aac.sampling_frequency_index == mpeg4_aac_audio_frequency_from(22050));
+
+	//assert(sizeof(ascsbr) == mpeg4_aac_audio_specific_config_load(ascsbr, sizeof(ascsbr), &aac));
+	//assert(2 == aac.profile && 6 == aac.sampling_frequency_index && 1 == aac.channel_configuration);
 }
 #endif
