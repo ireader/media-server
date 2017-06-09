f783850e5aca4691cd988d5850041811f327e5ca
ireader <ch.magus@163.com>
2017-06-09 11:30:19 +0800

    add FLV_AUDIO_ASC AudioSpecificConfig add FLV_VIDEO_AVCC AVCDecoderConfigurationRecord

--- libflv/include/flv-proto.h
+++ libflv/include/flv-proto.h
@@ -11,11 +11,13 @@
 #define FLV_AUDIO_MP3		2
 #define FLV_AUDIO_G711		7
 #define FLV_AUDIO_AAC		10
-#define FLV_AUDIO_MP3_8k	14
+#define FLV_AUDIO_MP3_8K	14
+#define FLV_AUDIO_ASC		16 // AudioSpecificConfig(ISO-14496-3)
 
 // FLV Video Type
 #define FLV_VIDEO_H263		2
 #define FLV_VIDEO_VP6		4
-#define FLV_VIDEO_AVC		7
+#define FLV_VIDEO_H264		7
+#define FLV_VIDEO_AVCC		16 // AVCDecoderConfigurationRecord(ISO-14496-15)
 
 #endif /* !_flv_proto_h_ */
