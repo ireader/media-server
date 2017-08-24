137fe025781c7c19fb5e369b0262533110a2c561
ireader <tao3@outlook.com>
2017-08-24 17:15:06 +0800

    srand call only once

--- librtp/source/rtp-ssrc.c
+++ librtp/source/rtp-ssrc.c
@@ -4,8 +4,11 @@
 
 int rtp_ssrc(void)
 {
-	int ssrc;
-	srand((unsigned int)time(NULL));
-	ssrc = rand();
-	return ssrc;
+	static unsigned int seed = 0;
+	if (0 == seed)
+	{
+		seed = (unsigned int)time(NULL);
+		srand(seed);
+	}
+	return rand();
 }
