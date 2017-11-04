78b6ecc92a24855120ad0f5079b99668eaa65ae6
ireader <tao3@outlook.com>
2017-11-04 15:27:20 +0800

    support h.265

--- libmov/test/mov-reader-test.cpp
+++ libmov/test/mov-reader-test.cpp
@@ -1,5 +1,6 @@
 #include "mov-reader.h"
 #include "mov-format.h"
+#include "mpeg4-hevc.h"
 #include "mpeg4-avc.h"
 #include "mpeg4-aac.h"
 #include <stdio.h>
@@ -10,18 +11,23 @@
 static uint8_t s_packet[2 * 1024 * 1024];
 static uint8_t s_buffer[4 * 1024 * 1024];
 static FILE *s_vfp, *s_afp;
+static struct mpeg4_hevc_t s_hevc;
 static struct mpeg4_avc_t s_avc;
 static struct mpeg4_aac_t s_aac;
-static uint32_t s_aac_track;
-static uint32_t s_avc_track;
-
-extern "C" size_t mpeg4_mp4toannexb(const struct mpeg4_avc_t* avc, const void* data, size_t bytes, void* out, size_t size);
+static uint32_t s_aac_track = 0xFFFFFFFF;
+static uint32_t s_avc_track = 0xFFFFFFFF;
+static uint32_t s_hevc_track = 0xFFFFFFFF;
 
 static void onread(void* flv, uint32_t track, const void* buffer, size_t bytes, int64_t pts, int64_t dts)
 {
 	if (s_avc_track == track)
 	{
 		int n = mpeg4_mp4toannexb(&s_avc, buffer, bytes, s_packet, sizeof(s_packet));
+		fwrite(s_packet, 1, n, s_vfp);
+	}
+	else if (s_hevc_track == track)
+	{
+		int n = hevc_mp4toannexb(&s_hevc, buffer, bytes, s_packet, sizeof(s_packet));
 		fwrite(s_packet, 1, n, s_vfp);
 	}
 	else if (s_aac_track == track)
@@ -39,17 +45,28 @@ static void onread(void* flv, uint32_t track, const void* buffer, size_t bytes, 
 
 static void mov_video_info(void* /*param*/, uint32_t track, uint8_t object, int /*width*/, int /*height*/, const void* extra, size_t bytes)
 {
-	s_avc_track = track;
-	assert(MOV_OBJECT_H264 == object);
-	mpeg4_avc_decoder_configuration_record_load((const uint8_t*)extra, bytes, &s_avc);
-
-	// write sps/pps
-	int n = mpeg4_avc_to_nalu(&s_avc, (uint8_t*)s_buffer, sizeof(s_buffer));
-	fwrite(s_buffer, 1, n, s_vfp);
+	if (MOV_OBJECT_H264 == object)
+	{
+		s_vfp = fopen("v.h264", "wb");
+		s_avc_track = track;
+		mpeg4_avc_decoder_configuration_record_load((const uint8_t*)extra, bytes, &s_avc);
+	}
+	else if (MOV_OBJECT_HEVC == object)
+	{
+		s_vfp = fopen("v.h265", "wb");
+		s_hevc_track = track;
+		mpeg4_hevc_decoder_configuration_record_load((const uint8_t*)extra, bytes, &s_hevc);
+	}
+	else
+	{
+		assert(0);
+	}
 }
 
 static void mov_audio_info(void* /*param*/, uint32_t track, uint8_t object, int channel_count, int /*bit_per_sample*/, int sample_rate, const void* /*extra*/, size_t /*bytes*/)
 {
+	s_afp = fopen("a.aac", "wb");
+
 	s_aac_track = track;
 	assert(MOV_OBJECT_AAC == object);
 	s_aac.profile = MPEG4_AAC_LC;
@@ -59,9 +76,6 @@ static void mov_audio_info(void* /*param*/, uint32_t track, uint8_t object, int 
 
 void mov_reader_test(const char* mp4)
 {
-	s_vfp = fopen("v.h264", "wb");
-	s_afp = fopen("a.aac", "wb");
-
 	mov_reader_t* mov = mov_reader_create(mp4);
 
 	mov_reader_getinfo(mov, mov_video_info, mov_audio_info, NULL);
@@ -71,6 +85,6 @@ void mov_reader_test(const char* mp4)
 	}
 
 	mov_reader_destroy(mov);
-	fclose(s_vfp);
-	fclose(s_afp);
+	if(s_vfp) fclose(s_vfp);
+	if(s_afp) fclose(s_afp);
 }
