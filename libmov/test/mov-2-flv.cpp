db889e275cad07ab22b394063aec37e060c85641
ireader <tao3@outlook.com>
2017-12-16 11:13:25 +0800

    add keyframe check

--- libmov/test/mov-2-flv.cpp
+++ libmov/test/mov-2-flv.cpp
@@ -2,29 +2,54 @@
 #include "mov-format.h"
 #include "mpeg4-avc.h"
 #include "mpeg4-aac.h"
+#include "flv-proto.h"
 #include "flv-writer.h"
+#include "flv-muxer.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 
-#define H264_NAL_IDR		5 // Coded slice of an IDR picture
-#define H264_NAL_SPS		7 // Sequence parameter set
-#define H264_NAL_PPS		8 // Picture parameter set
-
 static uint8_t s_packet[2 * 1024 * 1024];
 static uint8_t s_buffer[4 * 1024 * 1024];
-static struct mpeg4_avc_t s_avc;
 static uint32_t s_aac_track;
 static uint32_t s_avc_track;
+static uint32_t s_txt_track;
+static uint8_t s_video_type;
+
+static int iskeyframe(const uint8_t* data, size_t bytes)
+{
+	int nalu_length = 4;
+	uint32_t len;
+
+	while (bytes >= nalu_length + 1)
+	{
+		len = 0;
+		for (int i = 0; i < nalu_length; i++)
+			len = (len << 8) | data[i];
+
+		if (len + nalu_length > bytes)
+			return 0; // invalid
+
+		uint8_t nalu_type = (FLV_VIDEO_H264 == s_video_type) ? (data[nalu_length] & 0x1f) : ((data[nalu_length] >> 1) & 0x3f);
+		if ((FLV_VIDEO_H264 == s_video_type) ? (5 == nalu_type) : (16 <= nalu_type && nalu_type <= 23))
+			return 1;
+
+		bytes -= nalu_length + len;
+		data += nalu_length + len;
+	}
+
+	return 0;
+}
 
 static void onread(void* flv, uint32_t track, const void* buffer, size_t bytes, int64_t pts, int64_t dts)
 {
 	if (s_avc_track == track)
 	{
-		printf("[V] pts: %08lld, dts: %08lld\n", pts, dts);
+		int keyframe = iskeyframe((const uint8_t*)buffer, bytes);
 		int compositionTime = (int)(pts - dts);
-		s_packet[0] = (1 << 4) /* FrameType */ | 7 /* AVC */;
+		printf("[V] pts: %08lld, dts: %08lld%s\n", pts, dts, keyframe ? " [I]" : "");
+		s_packet[0] = ((keyframe ? 1 : 2) << 4) /* FrameType */ | s_video_type;
 		s_packet[1] = 1; // AVC NALU
 		s_packet[2] = (compositionTime >> 16) & 0xFF;
 		s_packet[3] = (compositionTime >> 8) & 0xFF;
@@ -49,8 +74,9 @@ static void onread(void* flv, uint32_t track, const void* buffer, size_t bytes, 
 static void mov_video_info(void* flv, uint32_t track, uint8_t object, int /*width*/, int /*height*/, const void* extra, size_t bytes)
 {
 	s_avc_track = track;
-	assert(MOV_OBJECT_H264 == object);
-	s_packet[0] = (1 << 4) /* FrameType */ | 7 /* AVC */;
+	assert(MOV_OBJECT_H264 == object || MOV_OBJECT_HEVC == object);
+	s_video_type = MOV_OBJECT_H264 == object ? FLV_VIDEO_H264 : FLV_VIDEO_H265;
+	s_packet[0] = (1 << 4) /* FrameType */ | s_video_type;
 	s_packet[1] = 0; // AVC sequence header
 	s_packet[2] = 0; // CompositionTime 0
 	s_packet[3] = 0;
@@ -77,12 +103,27 @@ static void mov_audio_info(void* flv, uint32_t track, uint8_t object, int /*chan
 #endif
 }
 
+static int mov_meta_info(void* flv, int type, const void* data, size_t bytes, uint32_t timestamp)
+{
+	return flv_writer_input(flv, FLV_TYPE_SCRIPT, data, bytes, 0);
+}
+
 void mov_2_flv_test(const char* mp4)
 {
 	snprintf((char*)s_packet, sizeof(s_packet), "%s.flv", mp4);
 
 	mov_reader_t* mov = mov_reader_create(mp4);
 	void* flv = flv_writer_create((char*)s_packet);
+
+	//flv_muxer_t* muxer = flv_muxer_create(mov_meta_info, flv);
+	//memset(&metadata, 0, sizeof(metadata));
+	//metadata.videocodecid = FLV_VIDEO_H264;
+	//metadata.videodatarate = 2000;
+	//metadata.framerate = 25.0;
+	//metadata.width = 1280;
+	//metadata.height = 720;
+	//flv_muxer_metadata(muxer, &metadata);
+	//flv_muxer_destroy(muxer);
 
 	mov_reader_getinfo(mov, mov_video_info, mov_audio_info, flv);
 
