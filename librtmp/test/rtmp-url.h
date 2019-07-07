626b92baa0666d6b51fd18762f8462751ea35e73
ireader <tao3@outlook.com>
2019-07-07 17:20:38 +0800

    add rtmp url parser

---
+++ librtmp/test/rtmp-url.h
@@ -0,0 +1,49 @@
+#ifndef _rtmp_url_h_
+#define _rtmp_url_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "uri-parse.h"
+
+#define URL_LENGTH 256
+
+struct rtmp_url_t
+{
+	unsigned short port; // default 1935
+	char scheme[32];
+	char host[URL_LENGTH];
+	char app[URL_LENGTH];
+	char stream[URL_LENGTH];
+	char tcurl[URL_LENGTH];
+};
+
+static int rtmp_url_parse(const char* url, struct rtmp_url_t* u)
+{
+	struct uri_t* uri = uri_parse(url, strlen(url));
+	if (!uri) return -1;
+	u->port = 0 == uri->port ? 1935 : uri->port;
+	snprintf(u->host, sizeof(u->host), "%s", uri->host);
+	snprintf(u->scheme, sizeof(u->scheme), "%s", uri->scheme);
+	uri_free(uri);
+
+	const char* p = strstr(url, "://");
+	p = p ? p + 3 : url;
+	p = strchr(p, '/');
+	if (!p) return -1;
+	p += 1;
+
+	const char* p1 = strchr(p, '/');
+	if (!p1) return -1;
+
+	if (p + sizeof(u->app) >= p1)
+		return -1;
+	memcpy(u->app, p, p1 - p);
+	u->app[p1 - p] = 0;
+
+	snprintf(u->stream, sizeof(u->stream), "%s", ++p1);
+	snprintf(u->tcurl, sizeof(u->tcurl), "rtmp://%s:%d/%s", u->host, u->port, u->app);
+	return 0;
+}
+
+#endif /* !_rtmp_url_h_ */
