1506da859bcd2be46282f6613ae8a4f46647e7b1
tao3 <tao3@outlook.com>
2020-08-15 10:37:38 +0800

    return error on unknown rtmp code

--- librtmp/source/rtmp-client-invoke-handler.h
+++ librtmp/source/rtmp-client-invoke-handler.h
@@ -217,6 +217,7 @@ static int rtmp_command_onstatus(struct rtmp_t* rtmp, double transaction, const 
 		{
 			assert(0);
 			printf("%s: level: %s, code: %s, description: %s\n", __FUNCTION__, result.level, result.code, result.description);
+			return -1;
 		}
 	}
 
