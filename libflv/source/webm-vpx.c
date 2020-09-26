f1d4923c65fe77bc8247a07bb87c5cc11b016d40
tao3 <tao3@outlook.com>
2020-09-26 08:35:29 +0800

    vpcc

--- libflv/source/webm-vpx.c
+++ libflv/source/webm-vpx.c
@@ -72,6 +72,53 @@ int webm_vpx_codec_configuration_record_save(const struct webm_vpx_t* vpx, uint8
     return 8 + vpx->codec_intialization_data_size;
 }
 
+// https://www.webmproject.org/vp9/mp4/
+// https://github.com/webmproject/vp9-dash
+// https://www.rfc-editor.org/rfc/pdfrfc/rfc6386.txt.pdf
+// https://storage.googleapis.com/downloads.webmproject.org/docs/vp9/vp9-bitstream-specification-v0.6-20160331-draft.pdf
+int webm_vpx_codec_configuration_record_from_vp8(struct webm_vpx_t* vpx, const void* keyframe, size_t bytes)
+{
+    int width, height;
+    uint32_t tag;
+    const uint8_t* p;
+    const uint8_t startcode[] = { 0x9d, 0x01, 0x2a };
+
+    if (bytes < 10)
+        return -1;
+
+    p = (const uint8_t*)keyframe;
+
+    // frame_tag
+    tag = (uint32_t)p[0] | ((uint32_t)p[1] << 8) | ((uint32_t)p[2] << 16);
+    //key_frame = tag & 0x01;
+    //version = (tag >> 1) & 0x07;
+    //show_frame = (tag >> 4) & 0x1;
+    //first_part_size = (tag >> 5) & 0x7FFFF;
+
+    if (0 == (tag & 0x01) || startcode[0] != p[3] || startcode[1] != p[4] || startcode[2] != p[5])
+        return -1; // not key frame
+
+    width = ((uint16_t)(p[6] & 0x3F) << 8) | (uint16_t)(p[7]); // (2 bits Horizontal Scale << 14) | Width (14 bits)
+    height = ((uint16_t)(p[8] & 0x3F) << 8) | (uint16_t)(p[9]); // (2 bits Vertical Scale << 14) | Height (14 bits)
+
+    memset(vpx, 0, sizeof(*vpx));
+    vpx->profile = (tag >> 1) & 0x03;
+    vpx->level = 31;
+    vpx->bit_depth = 8;
+    return 0;
+}
+
+// TODO:
+int webm_vpx_codec_configuration_record_from_vp9(struct webm_vpx_t* vpx, const void* keyframe, size_t bytes)
+{
+    (void)keyframe, (void)bytes;
+    memset(vpx, 0, sizeof(*vpx));
+    vpx->profile = 0;
+    vpx->level = 31;
+    vpx->bit_depth = 8;
+    return 0;
+}
+
 #if defined(_DEBUG) || defined(DEBUG)
 void webm_vpx_test(void)
 {
