c175f0f667281a6ccdc2dbbe27a56bae0f1373bf
tao3 <tao3@outlook.com>
2021-04-10 07:49:29 +0800

    avcodecid & flv codec id

--- librtsp/source/utils/rtsp-payloads.h
+++ librtsp/source/utils/rtsp-payloads.h
@@ -1,13 +1,24 @@
 #ifndef _rtsp_payloads_h_
 #define _rtsp_payloads_h_
 
+#include "flv-proto.h"
 #include "mov-format.h"
 #include "mkv-format.h"
 #include "rtp-profile.h"
 #include "mpeg-ts-proto.h"
 
+#ifdef _avcodecid_h_
+#define CODEC(avcodec, mov, mkv, flv, mpeg2, rtp, str) { avcodec, flv, mov, mpeg2, rtp, str, mkv }
+#else
+#define CODEC(avcodec, mov, mkv, flv, mpeg2, rtp, str) { flv, mov, mpeg2, rtp, str, mkv }
+#endif
+
 static struct
 {
+#ifdef _avcodecid_h_
+    AVPACKET_CODEC_ID codecid;
+#endif
+    uint8_t flv; // flv codec id
     uint8_t mov; // mov object id
     uint8_t mpeg2; // mpeg2 codecid
     uint8_t payload; // rtp payload id
@@ -15,19 +26,45 @@ static struct
     enum mkv_codec_t mkv; // mkv codec id
 } s_payloads[] = {
     // video
-    { MOV_OBJECT_H264,    PSI_STREAM_H264,       RTP_PAYLOAD_H264,   "H264",    MKV_CODEC_VIDEO_H264, },
-    { MOV_OBJECT_HEVC,    PSI_STREAM_H265,       RTP_PAYLOAD_H265,   "H265",    MKV_CODEC_VIDEO_H265, },
-    { MOV_OBJECT_VP8,     PSI_STREAM_VP8,        RTP_PAYLOAD_VP8,    "VP8",     MKV_CODEC_VIDEO_VP8,  },
-    { MOV_OBJECT_VP9,     PSI_STREAM_VP9,        RTP_PAYLOAD_VP9,    "VP9",     MKV_CODEC_VIDEO_VP9,  },
-    { MOV_OBJECT_AV1,     PSI_STREAM_AV1,        RTP_PAYLOAD_AV1,    "AV1",     MKV_CODEC_VIDEO_AV1,  },
+    CODEC(AVCODEC_VIDEO_H264,   MOV_OBJECT_H264,    MKV_CODEC_VIDEO_H264,   FLV_VIDEO_H264,     PSI_STREAM_H264,        RTP_PAYLOAD_H264,   "H264"),
+    CODEC(AVCODEC_VIDEO_H265,   MOV_OBJECT_HEVC,    MKV_CODEC_VIDEO_H265,   FLV_VIDEO_H265,     PSI_STREAM_H265,        RTP_PAYLOAD_H265,   "H265"),
+    CODEC(AVCODEC_VIDEO_VP8,    MOV_OBJECT_VP8,     MKV_CODEC_VIDEO_VP8,    0,                  PSI_STREAM_VP8,         RTP_PAYLOAD_VP8,    "VP8"),
+    CODEC(AVCODEC_VIDEO_VP9,    MOV_OBJECT_VP9,     MKV_CODEC_VIDEO_VP9,    0,                  PSI_STREAM_VP9,         RTP_PAYLOAD_VP9,    "VP9"),
+    CODEC(AVCODEC_VIDEO_AV1,    MOV_OBJECT_AV1,     MKV_CODEC_VIDEO_AV1,    FLV_VIDEO_AV1,      PSI_STREAM_VP9,         RTP_PAYLOAD_AV1,    "AV1"),
 
     // audio
-    { MOV_OBJECT_AAC,     PSI_STREAM_AAC,        RTP_PAYLOAD_MP4A,   "MP4A-LATM", MKV_CODEC_AUDIO_AAC,  },
-    { MOV_OBJECT_OPUS,    PSI_STREAM_AUDIO_OPUS, RTP_PAYLOAD_OPUS,   "opus",    MKV_CODEC_AUDIO_OPUS, },
-    { MOV_OBJECT_MP3,     PSI_STREAM_MP3,        RTP_PAYLOAD_MP3,    "",        MKV_CODEC_AUDIO_MP3,  }, // rtp standard payload id
-    { MOV_OBJECT_G711u,   PSI_STREAM_AUDIO_G711U,RTP_PAYLOAD_PCMU,   "",        MKV_CODEC_UNKNOWN,    }, // rtp standard payload id
-    { MOV_OBJECT_G711a,   PSI_STREAM_AUDIO_G711A,RTP_PAYLOAD_PCMA,   "",        MKV_CODEC_UNKNOWN,    }, // rtp standard payload id    
+    CODEC(AVCODEC_AUDIO_AAC,    MOV_OBJECT_AAC,     MKV_CODEC_AUDIO_AAC,    FLV_AUDIO_AAC,      PSI_STREAM_AAC,         RTP_PAYLOAD_MP4A,   "MP4A-LATM"),
+    CODEC(AVCODEC_AUDIO_OPUS,   MOV_OBJECT_OPUS,    MKV_CODEC_AUDIO_OPUS,   FLV_AUDIO_OPUS,     PSI_STREAM_AUDIO_OPUS,  RTP_PAYLOAD_OPUS,   "opus"),
+    CODEC(AVCODEC_AUDIO_MP3,    MOV_OBJECT_MP3,     MKV_CODEC_AUDIO_MP3,    FLV_AUDIO_MP3,      PSI_STREAM_MP3,         RTP_PAYLOAD_MP3,    ""), // rtp standard payload id
+    CODEC(AVCODEC_AUDIO_G711U,  MOV_OBJECT_G711u,   MKV_CODEC_UNKNOWN,      FLV_AUDIO_G711A,    PSI_STREAM_AUDIO_G711U, RTP_PAYLOAD_PCMU,   ""), // rtp standard payload id
+    CODEC(AVCODEC_AUDIO_G711A,  MOV_OBJECT_G711a,   MKV_CODEC_UNKNOWN,      FLV_AUDIO_G711U,    PSI_STREAM_AUDIO_G711A, RTP_PAYLOAD_PCMA,   ""), // rtp standard payload id    
 };
+
+#ifdef _avcodecid_h_
+inline int avcodecid_find_by_codecid(AVPACKET_CODEC_ID codecid)
+{
+    int i;
+    for (i = 0; i < sizeof(s_payloads) / sizeof(s_payloads[0]); i++)
+    {
+        if (s_payloads[i].codecid == codecid)
+            return i;
+    }
+
+    return -1;
+}
+#endif
+
+static inline int avpayload_find_by_flv(int flv)
+{
+    int i;
+    for (i = 0; i < sizeof(s_payloads) / sizeof(s_payloads[0]); i++)
+    {
+        if (int(s_payloads[i].flv) == flv)
+            return i;
+    }
+
+    return -1;
+}
 
 static inline int avpayload_find_by_mov(uint8_t object)
 {
