55cdeda746e01cd1edad3ae3f99146d410a712c7
tao3 <tao3@outlook.com>
2021-06-12 20:52:03 +0800

    fix Registration descriptor length check error

--- libmpeg/source/mpeg-element-descriptor.c
+++ libmpeg/source/mpeg-element-descriptor.c
@@ -145,72 +145,91 @@ size_t mpeg_elment_descriptor(const uint8_t* data, size_t bytes)
 size_t video_stream_descriptor(const uint8_t* data, size_t bytes)
 {
 	// 2.6.2 Video stream descriptor(p85)
-	size_t i;
-	video_stream_descriptor_t desc;
-//	uint8_t descriptor_tag = data[0];
-	size_t descriptor_len = data[1];
-	assert(descriptor_len+2 <= bytes);
 
-	i = 2;
+	uint8_t v;
+	size_t descriptor_len;
+	struct mpeg_bits_t bits;
+	video_stream_descriptor_t desc;
+
+	mpeg_bits_init(&bits, data, bytes);
+	mpeg_bits_read8(&bits); // descriptor_tag
+	descriptor_len = mpeg_bits_read8(&bits);
+	assert(descriptor_len + 2 <= bytes);
+
 	memset(&desc, 0, sizeof(desc));
-	desc.multiple_frame_rate_flag = (data[i] >> 7) & 0x01;
-	desc.frame_rate_code = (data[i] >> 3) & 0x0F;
-	desc.MPEG_1_only_flag = (data[i] >> 2) & 0x01;
-	desc.constrained_parameter_flag = (data[i] >> 1) & 0x01;
-	desc.still_picture_flag = data[i] & 0x01;
+	v = mpeg_bits_read8(&bits);
+	desc.multiple_frame_rate_flag = (v >> 7) & 0x01;
+	desc.frame_rate_code = (v >> 3) & 0x0F;
+	desc.MPEG_1_only_flag = (v >> 2) & 0x01;
+	desc.constrained_parameter_flag = (v >> 1) & 0x01;
+	desc.still_picture_flag = v & 0x01;
 
 	if(0 == desc.MPEG_1_only_flag)
 	{
-		desc.profile_and_level_indication = data[i+1];
-		desc.chroma_format = (data[i+2] >> 6) & 0x03;
-		desc.frame_rate_code = (data[i+2] >> 5) & 0x01;
-		assert((0x1F & data[i+2]) == 0x00); // 'xxx00000'
+		desc.profile_and_level_indication = mpeg_bits_read8(&bits);
+		v = mpeg_bits_read8(&bits);
+		desc.chroma_format = (v >> 6) & 0x03;
+		desc.frame_rate_code = (v >> 5) & 0x01;
+		assert((0x1F & v) == 0x00); // 'xxx00000'
 	}
 
+	assert(0 == mpeg_bits_error(&bits));
 	return descriptor_len+2;
 }
 
 size_t audio_stream_descriptor(const uint8_t* data, size_t bytes)
 {
 	// 2.6.4 Audio stream descriptor(p86)
-	size_t i;
+
+	uint8_t v;
+	size_t descriptor_len;
+	struct mpeg_bits_t bits;
 	audio_stream_descriptor_t desc;
-//	uint8_t descriptor_tag = data[0];
-	size_t descriptor_len = data[1];
-	assert(descriptor_len+2 <= bytes);
 
-	i = 2;
+	mpeg_bits_init(&bits, data, bytes);
+	mpeg_bits_read8(&bits); // descriptor_tag
+	descriptor_len = mpeg_bits_read8(&bits);
+	assert(descriptor_len + 2 <= bytes);
+
+	v = mpeg_bits_read8(&bits);
 	memset(&desc, 0, sizeof(desc));
-	desc.free_format_flag = (data[i] >> 7) & 0x01;
-	desc.ID = (data[i] >> 6) & 0x01;
-	desc.layer = (data[i] >> 4) & 0x03;
-	desc.variable_rate_audio_indicator = (data[i] >> 3) & 0x01;
+	desc.free_format_flag = (v >> 7) & 0x01;
+	desc.ID = (v >> 6) & 0x01;
+	desc.layer = (v >> 4) & 0x03;
+	desc.variable_rate_audio_indicator = (v >> 3) & 0x01;
 
-	assert(4 == descriptor_len);
+	assert(0 == mpeg_bits_error(&bits));
 	return descriptor_len+2;
 }
 
 size_t hierarchy_descriptor(const uint8_t* data, size_t bytes)
 {
 	// 2.6.6 Hierarchy descriptor(p86)
-	size_t i;
+	
+	uint8_t v;
+	size_t descriptor_len;
+	struct mpeg_bits_t bits;
 	hierarchy_descriptor_t desc;
-//	uint8_t descriptor_tag = data[0];
-	size_t descriptor_len = data[1];
-	assert(descriptor_len+2 <= bytes);
+	
+	mpeg_bits_init(&bits, data, bytes);
+	mpeg_bits_read8(&bits); // descriptor_tag
+	descriptor_len = mpeg_bits_read8(&bits);
+	assert(descriptor_len + 2 <= bytes);
 
-	i = 2;
+	v = mpeg_bits_read8(&bits);
 	memset(&desc, 0, sizeof(desc));
-	desc.temporal_scalability_flag = (data[i] >> 6) & 0x01;
-	desc.spatial_scalability_flag = (data[i] >> 5) & 0x01;
-	desc.quality_scalability_flag = (data[i] >> 4) & 0x01;
-	desc.hierarchy_type = data[i] & 0x0F;
-	desc.hierarchy_layer_index = data[i+1] & 0x3F;
-	desc.tref_present_flag = (data[i+2] >> 7) & 0x01;
-	desc.hierarchy_embedded_layer_index = data[i+2] & 0x3F;
-	desc.hierarchy_channel = data[i+3] & 0x3F;
+	desc.no_view_scalability_flag = (v >> 7) & 0x01;
+	desc.no_temporal_scalability_flag = (v >> 6) & 0x01;
+	desc.no_spatial_scalability_flag = (v >> 5) & 0x01;
+	desc.no_quality_scalability_flag = (v >> 4) & 0x01;
+	desc.hierarchy_type = v & 0x0F;
+	desc.hierarchy_layer_index = mpeg_bits_read8(&bits) & 0x3F;
+	v = mpeg_bits_read8(&bits);
+	desc.tref_present_flag = (v >> 7) & 0x01;
+	desc.hierarchy_embedded_layer_index = v & 0x3F;
+	desc.hierarchy_channel = mpeg_bits_read8(&bits) & 0x3F;
 
-	assert(4 == descriptor_len);
+	assert(0 == mpeg_bits_error(&bits));
 	return descriptor_len+2;
 }
 
@@ -218,10 +237,17 @@ size_t registration_descriptor(const uint8_t* data, size_t bytes)
 {
 	// 2.6.8 Registration descriptor(p94)
 	size_t fourcc;
-	//	uint8_t descriptor_tag = data[0];
-	size_t descriptor_len = data[1];
-	assert(descriptor_len + 2 + 4 <= bytes);
-	fourcc = (data[5] << 24) | (data[4] << 16) | (data[3] << 8) | data[2];
+	size_t descriptor_len;
+	struct mpeg_bits_t bits;
+
+	mpeg_bits_init(&bits, data, bytes);
+	mpeg_bits_read8(&bits); // descriptor_tag
+	descriptor_len = mpeg_bits_read8(&bits);
+	assert(descriptor_len + 2 <= bytes);
+
+	fourcc = mpeg_bits_read32(&bits);
+
+	assert(0 == mpeg_bits_error(&bits));
 	return descriptor_len + 2;
 }
 
@@ -233,7 +259,7 @@ size_t language_descriptor(const uint8_t* data, size_t bytes)
 	size_t descriptor_len = data[1];
 	assert(descriptor_len+2 <= bytes);
 
-	for(i = 2; i < descriptor_len; i += 4)
+	for(i = 2; i + 4 <= descriptor_len + 2; i += 4)
 	{
 		language_descriptor_t desc;
 		memset(&desc, 0, sizeof(desc));
@@ -248,53 +274,64 @@ size_t language_descriptor(const uint8_t* data, size_t bytes)
 size_t system_clock_descriptor(const uint8_t* data, size_t bytes)
 {
 	// 2.6.20 System clock descriptor(p92)
-	size_t i;
+
+	uint8_t v;
+	size_t descriptor_len;
+	struct mpeg_bits_t bits;
 	system_clock_descriptor_t desc;
-//	uint8_t descriptor_tag = data[0];
-	size_t descriptor_len = data[1];
-	assert(descriptor_len+2 <= bytes);
 
-	i = 2;
+	mpeg_bits_init(&bits, data, bytes);
+	mpeg_bits_read8(&bits); // descriptor_tag
+	descriptor_len = mpeg_bits_read8(&bits);
+	assert(descriptor_len + 2 <= bytes);
+
+	v = mpeg_bits_read8(&bits);
 	memset(&desc, 0, sizeof(desc));
-	desc.external_clock_reference_indicator = (data[i] >> 7) & 0x01;
-	desc.clock_accuracy_integer = data[i] & 0x3F;
-	desc.clock_accuracy_exponent = (data[i+1] >> 5) & 0x07;
+	desc.external_clock_reference_indicator = (v >> 7) & 0x01;
+	desc.clock_accuracy_integer = v & 0x3F;
+	desc.clock_accuracy_exponent = (mpeg_bits_read8(&bits) >> 5) & 0x07;
 
-	assert(2 == descriptor_len);
+	assert(0 == mpeg_bits_error(&bits));
 	return descriptor_len+2;
 }
 
 size_t mpeg4_video_descriptor(const uint8_t* data, size_t bytes)
 {
 	// 2.6.36 MPEG-4 video descriptor(p96)
-	size_t i;
+
+	size_t descriptor_len;
+	struct mpeg_bits_t bits;
 	mpeg4_video_descriptor_t desc;
-//	uint8_t descriptor_tag = data[0];
-	size_t descriptor_len = data[1];
-	assert(descriptor_len+2 <= bytes);
 
-	i = 2;
+	mpeg_bits_init(&bits, data, bytes);
+	mpeg_bits_read8(&bits); // descriptor_tag
+	descriptor_len = mpeg_bits_read8(&bits);
+	assert(descriptor_len + 2 <= bytes);
+
 	memset(&desc, 0, sizeof(desc));
-	desc.visual_profile_and_level = data[i];
+	desc.visual_profile_and_level = mpeg_bits_read8(&bits);
 
-	assert(1 == descriptor_len);
+	assert(0 == mpeg_bits_error(&bits));
 	return descriptor_len+2;
 }
 
 size_t mpeg4_audio_descriptor(const uint8_t* data, size_t bytes)
 {
 	// 2.6.38 MPEG-4 audio descriptor(p97)
-	size_t i;
+	
+	size_t descriptor_len;
+	struct mpeg_bits_t bits;
 	mpeg4_audio_descriptor_t desc;
-//	uint8_t descriptor_tag = data[0];
-	size_t descriptor_len = data[1];
-	assert(descriptor_len+2 <= bytes);
 
-	i = 2;
+	mpeg_bits_init(&bits, data, bytes);
+	mpeg_bits_read8(&bits); // descriptor_tag
+	descriptor_len = mpeg_bits_read8(&bits);
+	assert(descriptor_len + 2 <= bytes);
+
 	memset(&desc, 0, sizeof(desc));
-	desc.profile_and_level = data[i];
+	desc.profile_and_level = mpeg_bits_read8(&bits);
 
-	assert(1 == descriptor_len);
+	assert(0 == mpeg_bits_error(&bits));
 	return descriptor_len+2;
 }
 
@@ -396,131 +433,156 @@ size_t metadata_descriptor(const uint8_t* data, size_t bytes)
 size_t avc_video_descriptor(const uint8_t* data, size_t bytes)
 {
 	// 2.6.64 AVC video descriptor(p110)
-	size_t i;
+
+	uint8_t v;
+	size_t descriptor_len;
+	struct mpeg_bits_t bits;
 	avc_video_descriptor_t desc;
-//	uint8_t descriptor_tag = data[0];
-	size_t descriptor_len = data[1];
-	assert(descriptor_len+2 <= bytes);
+	
+	mpeg_bits_init(&bits, data, bytes);
+	mpeg_bits_read8(&bits); // descriptor_tag
+	descriptor_len = mpeg_bits_read8(&bits);
+	assert(descriptor_len + 2 <= bytes);
 
-	i = 2;
 	memset(&desc, 0, sizeof(desc));
-	desc.profile_idc = data[i];
-	desc.constraint_set0_flag = (data[i+1] >> 7) & 0x01;
-	desc.constraint_set1_flag = (data[i+1] >> 6) & 0x01;
-	desc.constraint_set2_flag = (data[i+1] >> 5) & 0x01;
-	desc.constraint_set3_flag = (data[i+1] >> 4) & 0x01;
-	desc.constraint_set4_flag = (data[i+1] >> 3) & 0x01;
-	desc.constraint_set5_flag = (data[i+1] >> 2) & 0x01;
-	desc.AVC_compatible_flags = data[i+1] & 0x03;
-	desc.level_idc = data[i+2];
-	desc.AVC_still_present = (data[i+3] >> 7) & 0x01;
-	desc.AVC_24_hour_picture_flag = (data[i+3] >> 6) & 0x01;
-	desc.frame_packing_SEI_not_present_flag = (data[i+3] >> 5) & 0x01;
+	desc.profile_idc = mpeg_bits_read8(&bits);
+	v = mpeg_bits_read8(&bits);
+	desc.constraint_set0_flag = (v >> 7) & 0x01;
+	desc.constraint_set1_flag = (v >> 6) & 0x01;
+	desc.constraint_set2_flag = (v >> 5) & 0x01;
+	desc.constraint_set3_flag = (v >> 4) & 0x01;
+	desc.constraint_set4_flag = (v >> 3) & 0x01;
+	desc.constraint_set5_flag = (v >> 2) & 0x01;
+	desc.AVC_compatible_flags = v & 0x03;
+	desc.level_idc = mpeg_bits_read8(&bits);
+	v = mpeg_bits_read8(&bits);
+	desc.AVC_still_present = (v >> 7) & 0x01;
+	desc.AVC_24_hour_picture_flag = (v >> 6) & 0x01;
+	desc.frame_packing_SEI_not_present_flag = (v >> 5) & 0x01;
 
-	assert(4 == descriptor_len);
+	assert(0 == mpeg_bits_error(&bits));
 	return descriptor_len+2;
 }
 
 size_t avc_timing_hrd_descriptor(const uint8_t* data, size_t bytes)
 {
 	// 2.6.66 AVC timing and HRD descriptor(p112)
-	size_t i;
-	avc_timing_hrd_descriptor_t desc;
-//	uint8_t descriptor_tag = data[0];
-	size_t descriptor_len = data[1];
-	assert(descriptor_len+2 <= bytes);
 
-	i = 2;
+	uint8_t v;
+	size_t descriptor_len;
+	struct mpeg_bits_t bits;
+	avc_timing_hrd_descriptor_t desc;
+
+	mpeg_bits_init(&bits, data, bytes);
+	mpeg_bits_read8(&bits); // descriptor_tag
+	descriptor_len = mpeg_bits_read8(&bits);
+	assert(descriptor_len + 2 <= bytes);
+
 	memset(&desc, 0, sizeof(desc));
-	desc.hrd_management_valid_flag = (data[i] >> 7) & 0x01;
-	desc.picture_and_timing_info_present = (data[i] >> 0) & 0x01;
-	++i;
+	v = mpeg_bits_read8(&bits);
+	desc.hrd_management_valid_flag = (v >> 7) & 0x01;
+	desc.picture_and_timing_info_present = (v >> 0) & 0x01;
 	if(desc.picture_and_timing_info_present)
 	{
-		desc._90kHZ_flag = (data[i] >> 7) & 0x01;
+		v = mpeg_bits_read8(&bits);
+		desc._90kHZ_flag = (v >> 7) & 0x01;
 		if(0 == desc._90kHZ_flag)
 		{
-			desc.N = (data[i+1] << 24) | (data[i+2] << 16) | (data[i+3] << 8) | data[i+4];
-			desc.K = (data[i+5] << 24) | (data[i+6] << 16) | (data[i+7] << 8) | data[i+8];
-			i += 8;
+			desc.N = mpeg_bits_read32(&bits);
+			desc.K = mpeg_bits_read32(&bits);
 		}
-		desc.num_unit_in_tick = (data[i+1] << 24) | (data[i+2] << 16) | (data[i+3] << 8) | data[i+4];
-		i += 5;
+		desc.num_unit_in_tick = mpeg_bits_read32(&bits);
 	}
 
-	desc.fixed_frame_rate_flag = (data[i] >> 7) & 0x01;
-	desc.temporal_poc_flag = (data[i] >> 6) & 0x01;
-	desc.picture_to_display_conversion_flag = (data[i] >> 5) & 0x01;
+	v = mpeg_bits_read8(&bits);
+	desc.fixed_frame_rate_flag = (v >> 7) & 0x01;
+	desc.temporal_poc_flag = (v >> 6) & 0x01;
+	desc.picture_to_display_conversion_flag = (v >> 5) & 0x01;
 
+	assert(0 == mpeg_bits_error(&bits));
 	return descriptor_len+2;
 }
 
 size_t mpeg2_aac_descriptor(const uint8_t* data, size_t bytes)
 {
 	// 2.6.68 MPEG-2 AAC audio descriptor(p113)
-	size_t i;
+
+	size_t descriptor_len;
+	struct mpeg_bits_t bits;
 	mpeg2_aac_descriptor_t desc;
-//	uint8_t descriptor_tag = data[0];
-	size_t descriptor_len = data[1];
-	assert(descriptor_len+2 <= bytes);
+	
+	mpeg_bits_init(&bits, data, bytes);
+	mpeg_bits_read8(&bits); // descriptor_tag
+	descriptor_len = mpeg_bits_read8(&bits);
+	assert(descriptor_len + 2 <= bytes);
 
-	i = 2;
 	memset(&desc, 0, sizeof(desc));
-	desc.profile = data[i];
-	desc.channel_configuration = data[i+1];
-	desc.additional_information = data[i+2];
+	desc.profile = mpeg_bits_read8(&bits);
+	desc.channel_configuration = mpeg_bits_read8(&bits);
+	desc.additional_information = mpeg_bits_read8(&bits);
 
-	assert(3 == descriptor_len);
+	assert(0 == mpeg_bits_error(&bits));
 	return descriptor_len+2;
 }
 
 size_t svc_extension_descriptor(const uint8_t* data, size_t bytes)
 {
 	// 2.6.76 SVC extension descriptor(p116)
-	size_t i;
+
+	uint8_t v;
+	size_t descriptor_len;
+	struct mpeg_bits_t bits;
 	svc_extension_descriptor_t desc;
-//	uint8_t descriptor_tag = data[0];
-	size_t descriptor_len = data[1];
-	assert(descriptor_len+2 <= bytes);
 
-	i = 2;
+	mpeg_bits_init(&bits, data, bytes);
+	mpeg_bits_read8(&bits); // descriptor_tag
+	descriptor_len = mpeg_bits_read8(&bits);
+	assert(descriptor_len + 2 <= bytes);
+
 	memset(&desc, 0, sizeof(desc));
-	desc.width = (data[i] << 8) | data[i+1];
-	desc.height = (data[i+2] << 8) | data[i+3];
-	desc.frame_rate = (data[i+4] << 8) | data[i+5];
-	desc.average_bitrate = (data[i+6] << 8) | data[i+7];
-	desc.maximum_bitrate = (data[i+8] << 8) | data[i+9];
-	desc.dependency_id = (data[i+10] >> 5) & 0x07;
-	desc.quality_id_start = (data[i+11] >> 4) & 0x0F;
-	desc.quality_id_end = (data[i+11] >> 0) & 0x0F;
-	desc.temporal_id_start = (data[i+12] >> 5) & 0x07;
-	desc.temporal_id_end = (data[i+12] >> 2) & 0x07;
-	desc.no_sei_nal_unit_present = (data[i+12] >> 1) & 0x01;
+	desc.width = mpeg_bits_read16(&bits);
+	desc.height = mpeg_bits_read16(&bits);
+	desc.frame_rate = mpeg_bits_read16(&bits);
+	desc.average_bitrate = mpeg_bits_read16(&bits);
+	desc.maximum_bitrate = mpeg_bits_read16(&bits);
+	desc.dependency_id = (mpeg_bits_read8(&bits) >> 5) & 0x07;
+	v = mpeg_bits_read8(&bits);
+	desc.quality_id_start = (v >> 4) & 0x0F;
+	desc.quality_id_end = (v >> 0) & 0x0F;
+	v = mpeg_bits_read8(&bits);
+	desc.temporal_id_start = (v >> 5) & 0x07;
+	desc.temporal_id_end = (v >> 2) & 0x07;
+	desc.no_sei_nal_unit_present = (v >> 1) & 0x01;
 
-	assert(13 == descriptor_len);
+	assert(0 == mpeg_bits_error(&bits));
 	return descriptor_len+2;
 }
 
 size_t mvc_extension_descriptor(const uint8_t* data, size_t bytes)
 {
 	// 2.6.78 MVC extension descriptor(p117)
-	size_t i;
+
+	uint32_t v;
+	size_t descriptor_len;
+	struct mpeg_bits_t bits;
 	mvc_extension_descriptor_t desc;
-//	uint8_t descriptor_tag = data[0];
-	size_t descriptor_len = data[1];
-	assert(descriptor_len+2 <= bytes);
 
-	i = 2;
+	mpeg_bits_init(&bits, data, bytes);
+	mpeg_bits_read8(&bits); // descriptor_tag
+	descriptor_len = mpeg_bits_read8(&bits);
+	assert(descriptor_len + 2 <= bytes);
+
 	memset(&desc, 0, sizeof(desc));
-	desc.average_bit_rate = (data[i] << 8) | data[i+1];
-	desc.maximum_bitrate = (data[i+2] << 8) | data[i+3];
-	desc.view_order_index_min = ((data[i+4] & 0xF) << 6) | ((data[i+5] >> 2) & 0x3F);
-	desc.view_order_index_max = ((data[i+5] & 0x3) << 8) | data[i+6];
-	desc.temporal_id_start = (data[i+7] >> 5) & 0x07;
-	desc.temporal_id_end = (data[i+7] >> 2) & 0x07;
-	desc.no_sei_nal_unit_present = (data[i+7] >> 1) & 0x01;
-	desc.no_prefix_nal_unit_present = (data[i+7] >> 0) & 0x01;
+	desc.average_bit_rate = mpeg_bits_read16(&bits);
+	desc.maximum_bitrate = mpeg_bits_read16(&bits);
+	v = mpeg_bits_read32(&bits);
+	desc.view_order_index_min = (v >> 18) & 0x3FF;
+	desc.view_order_index_max = (v >> 8) & 0x3FF;
+	desc.temporal_id_start = (v >> 5) & 0x07;
+	desc.temporal_id_end = (v >> 2) & 0x07;
+	desc.no_sei_nal_unit_present = (v >> 1) & 0x01;
+	desc.no_prefix_nal_unit_present = (v >> 0) & 0x01;
 
-	assert(8 == descriptor_len);
-	return descriptor_len+2;
+	assert(0 == mpeg_bits_error(&bits));
+	return descriptor_len + 2;
 }
